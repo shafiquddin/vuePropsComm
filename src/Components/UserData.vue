34522ef42951a6a5f5e7521e476ffd05472311ee
Aamer Sohail <amersohailroot@gmail.com>
2023-02-16 15:20:46 +0530

    initial Commit

---
+++ src/Components/UserData.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="AddDetails">
+        <div>
+            <label>Name:</label>
+            <input type="text" v-model="name">
+        </div>
+        <div>
+            <label>Age:</label>
+            <input type="text" v-model="age">
+        </div>
+        <button>Add User</button>
+    </form>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return {
+            name:'',
+            age:'',
+        }
+    },
+    methods:{
+        AddDetails(){
+            this.$emit('add-details',this.name,this.age);
+        } 
+    }
+}
+
+</script>
